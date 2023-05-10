1f2c1b83ce791067b93ff5e185204f60cab5efa3
MariaPetrukhnova <110037119+MariaPetrukhnova@users.noreply.github.com>
2023-05-10 10:09:51 +0300

    add fonts

--- tweets-cards/src/index.js
+++ tweets-cards/src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import 'typeface-montserrat';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
